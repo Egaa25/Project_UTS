91589b08d9deaf390c9e64f0893a4ae4f8934539
Ega Ananta <egaananta2004@.com>
2024-10-23 09:23:30 +0700

    lebih ringkas

--- project-uts/src/pages/Home.jsx
+++ project-uts/src/pages/Home.jsx
@@ -1,28 +1,55 @@
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import Tutors from "../components/Tutors";
-import Partners from "../components/Partners";
-import Contact from "../components/Contact";
-
-import parse from "html-react-parser";
-
-import { homeSection } from "../data/homeSection";
-import { coursesSection } from "../data/coursesSection";
-import { tutorsSection, tutorsList } from "../data/tutorsSection";
-import { partnerSection, partnerList } from "../data/partnerSection";
-import { contactSection } from "../data/contactSection";
+import logo from "../assets/images/piscokpng.png";
+import sosis from "../assets/images/sosis.png";
+import sigma from "../assets/images/Logoutspweb-06.png";
 
 import "../styles/Home.css";
 function Home() {
   return (
     <>
-      <Navbar />
+      {/* navbar */}
+      <nav>
+        <div className="wrapper">
+          <div className="logo">
+            <a href="">
+              <img src={sigma} alt="logo" className="logoPng" />
+            </a>
+          </div>
+          <div className="menu">
+            <ul>
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#menu">Menu</a>
+              </li>
+              <li>
+                <a href="#about">About us</a>
+              </li>
+              <li>
+                <a href="#Contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
       {/* Hero */}
       <div className="wrapper">
-        <section id="home">
-          <img src={homeSection.image} />
-          <div className="kolom">{parse(homeSection.content)}</div>
-        </section>
+        <div className="bungkus">
+          <section id="home">
+            <img src={logo} alt="logo" className="piscok" />
+            <div className="kolom">
+              <p className="deskripsi">Hi, Welcome to Sigma Store!</p>
+              <h2>Find the product that you want</h2>
+              <p>Order the products you want easily here</p>
+              <p>
+                <a href="" className="tbl-pink">
+                  Order now
+                </a>
+              </p>
+            </div>
+          </section>
+        </div>
 
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path
@@ -32,30 +59,36 @@ function Home() {
           ></path>
         </svg>
 
-        {/* Tutors Section */}
-        <section id="tutors">
-          <div className="tengah">
-            <div className="kolom">{parse(tutorsSection.content)}</div>
-            <Tutors tutorsList={tutorsList} />
-          </div>
-        </section>
-
-        {/* Course Section */}
+        {/* about us Section */}
         <section id="courses">
-          <div className="kolom">{parse(coursesSection.konten)}</div>
-          <img src={coursesSection.image} />
-        </section>
-
-        {/* partners */}
-        <section id="partners">
-          <div className="tengah">
-            <div className="kolom">{parse(partnerSection.content)}</div>
+          <div>
+            <h2>About Us</h2>
+            <p>
+              Welcome to Sigma Store, a place where you can fulfill your needs
+              for a variety of excellent products such as food and beverages.
+              From felicious snacks and refreshing drinks to exclusive items, we
+              provide the best products that support bussiness fund programs
+              (danus).
+            </p>
+            <p>
+              We believe that every product we offer at Sigma Store well hel you
+              in your activities, wheter for events, meetings, or daily needs.
+              Therefore, we are committed to quality, affordable prices, and
+              friendly customer service.
+            </p>
+            <p>
+              Thank you for trusting us to handle your needs; Sigma Store is
+              more than just a store.
+            </p>
           </div>
+          <img src={sosis} alt="sosis" className="sosis" />
         </section>
-        <Partners partnerList={partnerList} />
       </div>
-      <Contact contactSection={contactSection} />
-      <Footer />
+      <div id="copyright">
+        <div className="wrapper">
+          &copy; 2024. <b>Project UTS by Kelompok Sigma</b> All Rights Reserved.
+        </div>
+      </div>
     </>
   );
 }
