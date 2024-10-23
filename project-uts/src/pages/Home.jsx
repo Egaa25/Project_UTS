a2d27417706118188681fb2a702fb613de959118
Ega Ananta <egaananta2004@.com>
2024-10-23 13:54:51 +0700

    tinggal nunggu db

--- project-uts/src/pages/Home.jsx
+++ project-uts/src/pages/Home.jsx
@@ -14,6 +14,7 @@ function Home() {
             <a href="#">
               <img src={sigma} alt="logo" className="logoPng" />
             </a>
+            <h2 className="sigmaStore">SIGMA STORE</h2>
           </div>
           <div className="menu">
             <ul>
@@ -45,12 +46,24 @@ function Home() {
               <p>Order the products you want easily here</p>
               <p>
                 <Link to="/menu" className="tbl-pink">
-                  Order now
+                  View our menu
                 </Link>
               </p>
             </div>
           </section>
         </div>
+
+        <svg
+          className="wave1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+        >
+          <path
+            fill="#000b76"
+            fillOpacity="1"
+            d="M0,160L60,176C120,192,240,224,360,202.7C480,181,600,107,720,90.7C840,75,960,117,1080,128C1200,139,1320,117,1380,106.7L1440,96L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+          ></path>
+        </svg>
 
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path
@@ -84,11 +97,76 @@ function Home() {
           </div>
           <img src={sosis} alt="sosis" className="sosis" />
         </section>
+
+        {/* Card List Menu */}
+        <h2 className="ourMenu">Best Seller</h2>
+        <section id="menu" className="menu-section">
+          <div className="card-list">
+            <div className="card">
+              <img src={sosis} alt="Burger" className="card-image" />
+              <div className="card-content">
+                <h3>Burger</h3>
+                <p>Juicy beef burger with cheese, lettuce, and tomato.</p>
+                <p className="card-price">Rp 2.500 / pcs</p>
+                <Link to="/menu" className="tbl-pink">
+                  Order now
+                </Link>
+              </div>
+            </div>
+            <div className="card">
+              <img src={sosis} alt="Burger" className="card-image" />
+              <div className="card-content">
+                <h3>Burger</h3>
+                <p>Juicy beef burger with cheese, lettuce, and tomato.</p>
+                <p className="card-price">Rp 2.500 / pcs</p>
+                <Link to="/menu" className="tbl-pink">
+                  Order now
+                </Link>
+              </div>
+            </div>
+            <div className="card">
+              <img src={sosis} alt="Pizza" className="card-image" />
+              <div className="card-content">
+                <h3>Pizza</h3>
+                <p>Delicious cheese pizza with a variety of toppings.</p>
+                <p className="card-price">Rp 2.500 / pcs</p>
+                <Link to="/menu" className="tbl-pink">
+                  Order now
+                </Link>
+              </div>
+            </div>
+            <div className="card">
+              <img src={sosis} alt="Sushi" className="card-image" />
+              <div className="card-content">
+                <h3>Sushi</h3>
+                <p>Fresh sushi rolls with tuna, salmon, and avocado.</p>
+                <p className="card-price">Rp 2.500 / pcs</p>
+                <Link to="/menu" className="tbl-pink">
+                  Order now
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
+      <svg
+        className="wavefoot"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+      >
+        <path
+          fill="#272363"
+          fillOpacity="1"
+          d="M0,160L1440,256L1440,0L0,0Z"
+        ></path>
+      </svg>
       <div id="copyright">
         <div className="wrapper">
           &copy; 2024. <b>Project UTS by Kelompok Sigma</b> All Rights Reserved.
         </div>
+      </div>
+      <div className="logfoot">
+        <img src={sigma} alt="logo" className="logofoot" />
       </div>
     </>
   );
---
+++ project-uts/src/pages/menu.jsx
@@ -0,0 +1,15 @@
+// src/pages/Menu.js
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function Menu() {
+  return (
+    <div>
+      <h1>Our Menu</h1>
+      <p>Here is the list of products available in our store.</p>
+      {/* Tambahkan konten yang sesuai dengan daftar menu */}
+    </div>
+  );
+}
+
+export default Menu;
